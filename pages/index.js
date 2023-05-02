eaa90c90a35d9deb85ede6474084f40d5d731b04
Hawkeye <36890222+capmahin@users.noreply.github.com>
2023-05-03 05:58:38 +0600

    add w-full item center and text center

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
 
 export default function Home() {
   return (
-    <div className="bg-blue-900 w-screen h-screen flex"><div>
+    <div className="bg-blue-900 w-screen h-screen flex items-center"><div className="text-center w-full">
       <button className="bg-white p-2">Login with Google</button>
       </div></div>
   )
