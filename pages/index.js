3379721dd4474d044a5645e0eb6252ad1aaab66b
Hawkeye <36890222+capmahin@users.noreply.github.com>
2023-05-03 05:57:26 +0600

    add button for google loginn

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,8 @@
 
 export default function Home() {
   return (
-    <div className="bg-blue-900 w-screen h-screen">test</div>
+    <div className="bg-blue-900 w-screen h-screen flex"><div>
+      <button className="bg-white p-2">Login with Google</button>
+      </div></div>
   )
 }
