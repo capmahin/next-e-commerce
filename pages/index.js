6d07fdd3b80ac56f2b635d7d043761f90b1b9584
Hawkeye <36890222+capmahin@users.noreply.github.com>
2023-05-03 05:55:50 +0600

    working on admin part

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,6 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
-import styles from '@/styles/Home.module.css'
-
-const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return (
-    <div className={'bg-red-500'}>test</div>
+    <div className="bg-blue-900 w-screen h-screen">test</div>
   )
 }
